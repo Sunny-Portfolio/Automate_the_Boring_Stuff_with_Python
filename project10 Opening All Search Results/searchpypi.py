c7699d9ace5b2fef5b072dc2c250e75930a31dd8
Sunny <107274023+Sunny-Portfolio@users.noreply.github.com>
2024-05-23 16:10:55 -0400

    Completed fixed links

--- project10 Opening All Search Results/searchpypi.py
+++ project10 Opening All Search Results/searchpypi.py
@@ -4,13 +4,18 @@
 import requests, sys, webbrowser, bs4
 
 print('Searching...') # display text while downloading the search result page
-res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
-res.raise_for_status()
+res = requests.get('https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
+try:
+    res.raise_for_status()
+except Exception as e:
+    print('Something went wrong: %s' % e)
 
 # Retrieve top search result links.
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
 # Open a browser tab for each result.
 linkElems = soup.select('.package-snippet')
+print('length of elements:', len(linkElems))
 numOpen = min(5, len(linkElems))
 for i in range(numOpen):
     urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
